ed5240fed0d248226c7a0a1b8172ee63f26a15ec
Charles An <mucollabo@gmail.com>
2020-07-31 19:00:58 +0900

    until React Component make

--- src/App.js
+++ src/App.js
@@ -1,65 +1,49 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-function Food ({name, picture, rating}) {
-  
-  return (
-    <div>
-      <h2>I like {name} </h2>
-      <h4>{rating}/5.0</h4>
-      <img src={picture} alt={name} />
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor(props) {
+    super (props);
+    console.log ('hello');
+  }
 
-const foodILike = [
-  {
-    id: 1,
-    name: 'Pizza',
-    image: 'https://images.unsplash.com/photo-1574071318508-1cdbab80d002?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
-    rating: 5,
-  },
-  {
-    id: 2,
-    name: 'Salad',
-    image: 'https://images.unsplash.com/photo-1570197571499-166b36435e9f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
-    rating: 4.5,
-  },
-  {
-    id: 3,
-    name: 'Pasta',
-    image: 'https://images.unsplash.com/photo-1563379926898-05f4575a45d8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
-    rating: 4.2,
-  },
-  {
-    id: 4,
-    name: 'Kimchi',
-    image: 'https://images.unsplash.com/photo-1583224944844-5b268c057b72?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
-    rating: 5,
-  },
-  {
-    id: 5,
-    name: 'Cola',
-    image: 'https://images.unsplash.com/photo-1554169360-f86ef83314ec?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=400&q=80',
-    rating: 3.7,
-  },
-];
+  componentDidMount() {
+    console.log('component redered');
+  }
 
-function App() {
-  return (
-    <div>
-      
-      {foodILike.map(dish => (
-        <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />
-      ))}
-    </div>
-  );
-}
+  componentDidUpdate() {
+    console.log('I just updated');
+  }
 
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number,
+  componentWillUnmount() {
+    console.log('Goodbye, cruel world');
+  }
+
+  state = {
+    count: 0,
+  };
+
+  add = () => {
+    this.setState(current => ({
+      count: current.count + 1,
+    }));
+  };
+
+  minus = () => {
+    this.setState(current => ({
+      count: current.count - 1,
+    }));
+  };
+
+  render() {
+    console.log("I'm rendering");
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
